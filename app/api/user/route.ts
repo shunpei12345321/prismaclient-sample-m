e77b2b3f0925aa8c835654d33571342be13e470a
shunpei12345321 <u-shunpei-2404@occ.co.jp>
2024-07-16 16:24:39 +0900

    first commit

---
+++ app/api/user/route.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/PrismaClient";
+
+export const POST = async (req: Request, res: NextResponse) => {
+  const { name, email } = await req.json();
+
+  const new_user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+  return NextResponse.json(new_user);
+};
+
+export const GET = async (req: Request, res: NextResponse) => {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
+};
