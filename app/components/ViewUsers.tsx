ad6465039827f9ee6a49289a9ac6a7176085d0e8
shunpei12345321 <u-shunpei-2404@occ.co.jp>
2024-12-09 17:47:06 +0900

    tody-last

--- app/components/ViewUsers.tsx
+++ app/components/ViewUsers.tsx
@@ -8,7 +8,9 @@ const ViewUsers = () => {
 	const [users, setUsers] = useState<UserType[]>([]);
 	// const [reload, setReload] = useState(false);
 	const [isLoading, setIsLoading] = useState(false);
+	const [timers, setTimers] = useState<{ [key: string]: number }>({});
 
+	//これをリポジトリ―に持っていけばよい
 	useEffect(() => {
 		const fetchUsers = async () => {
 			setIsLoading(true);
@@ -22,9 +24,31 @@ const ViewUsers = () => {
 		fetchUsers();
 	}, []);
 
+	// タイマーをスタートする関数 １２０９
+	const startTimer = (userId: string) => {
+		setTimers((prevTimers) => ({
+			...prevTimers,
+			[userId]: 0, // 初期値として0秒からスタート
+		}));
+	};
+
 	// const handleReload = () => {
 	// 	setReload(!reload);
 	// };
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTimers((prevTimers) => {
+				const updatedTimers: { [key: string]: number } = {};
+				Object.keys(prevTimers).forEach((userId) => {
+					updatedTimers[userId] = prevTimers[userId] + 1;
+				});
+				return updatedTimers;
+			});
+		}, 1000);
+
+		return () => clearInterval(interval); // クリーンアップ
+	}, []);
 
 	return (
 		<div className="w-1/2 flex flex-col ">
@@ -42,15 +66,29 @@ const ViewUsers = () => {
 					</button>
 				)} */}
 			</div>
-			<div className="flex flex-col items-center justify-start">
+			{/* <div className="flex flex-col items-center justify-start">
+				{users.length > 0 && (
+					<div className="flex border-2 w-full px-2 py-1">
+						<p className="text-sm text-gray-800">
+							id{users[users.length - 1].id}
+						</p>
+						<p className="text-sm text-gray-800">
+							名前{users[users.length - 1].name}
+						</p>
+						<p className="text-sm text-gray-800">
+							メール{users[users.length - 1].email}
+						</p>
+					</div>
+				)}
+			</div> */}
+
+			<div className="flex flex-col items-center justify-start h-64 overflow-y-scroll border">
 				{users.map((user) => (
-					<button
-						key={user.id}
-						//href={`/user/edit/${user.id}`}
-						className="flex border-2 w-full px-2 py-1"
-					>
-						{JSON.stringify(user)}
-					</button>
+					<div key={user.id} className="flex border-b w-full px-2 py-1">
+						<p className="text-sm text-gray-800">id: {user.id}</p>
+						<p className="text-sm text-gray-800 ml-4">名前: {user.name}</p>
+						<p className="text-sm text-gray-800 ml-4">メール: {user.email}</p>
+					</div>
 				))}
 			</div>
 		</div>
